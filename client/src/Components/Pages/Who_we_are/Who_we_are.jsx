27848e29842700a278f00ba72226462698356b1d
Ordogen <oarcilagarcia@gmail.com>
2025-05-21 15:59:37 -0500

    vista quienes somos realizada

--- client/src/Components/Pages/Who_we_are/Who_we_are.jsx
+++ client/src/Components/Pages/Who_we_are/Who_we_are.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import './Who_we_are.css'
+import { Header } from "../../Layouts/Header/Header";
+import { Footer } from "../../Layouts/Footer/Footer";
+import { Main } from "../../Layouts/Main/Main";
+import ilustration_01 from "../../../assets/Ilustrations/Frame01.svg";
+import "./Who_we_are.css";
 
 export const Who_we_are = () => {
-    return (
-        <div className='div'>
-            QUIENES SOMOS
-        </div>
-    )
-}
+  return (
+    <div className="who_we_are">
+      <Header />
+      <Main>
+        <picture className="ilustration_who">
+          <img
+            className="image_who"
+            src={ilustration_01}
+            alt="ilustration 01"
+          />
+        </picture>
+        <section className="container_description_who">
+          <h1 className="title_who">
+            ¿Quiénes <span className="span_somos">somos</span>?
+          </h1>
+          <p className="description_who">
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
+            de las industrias desde el año 1500, cuando un impresor (N. del T.
+            persona que se dedica a la imprenta) desconocido usó una galería de
+            textos y los mezcló de tal manera que logró hacer un libro de
+            textosespecimen.
+          </p>
+        </section>
+      </Main>
+      <Footer />
+    </div>
+  );
+};
